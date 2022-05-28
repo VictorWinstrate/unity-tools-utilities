aad9e00178c9f6dbdcc6b264e58a042e2a8e1cf2
DarkFroggy658 <ferdric5697@gmail.com>
2022-05-28 17:00:02 +0800

    Update tab spacing
    In ComponentExtensions.cs and EnumExtensions.cs.

--- Scripts/Extensions/ComponentExtensions.cs
+++ Scripts/Extensions/ComponentExtensions.cs
@@ -2,15 +2,15 @@ using UnityEngine;
 
 public static class ComponentExtensions
 {
-    public static bool TryToGetComponent<T>(GameObject gameObject, out T instance) where T : Component
-    {
-        instance = gameObject.GetComponent<T>();
-        return instance != null;
-    }
+	public static bool TryToGetComponent<T>(GameObject gameObject, out T instance) where T : Component
+	{
+		instance = gameObject.GetComponent<T>();
+		return instance != null;
+	}
 
-    public static bool TryToGetComponent<T>(Component component, out T instance) where T : Component
-    {
-        instance = component.GetComponent<T>();
-        return instance != null;
-    }
+	public static bool TryToGetComponent<T>(Component component, out T instance) where T : Component
+	{
+		instance = component.GetComponent<T>();
+		return instance != null;
+	}
 }
--- Scripts/Extensions/EnumExtensions.cs
+++ Scripts/Extensions/EnumExtensions.cs
@@ -2,62 +2,62 @@ using System;
 
 public static class EnumExtensions
 {
-    const string _argumentExceptionFormat = "{0} must be an enumerated type";
+	const string _argumentExceptionFormat = "{0} must be an enumerated type";
 
-    public static bool HasNext<T>(this T src) where T : struct, IConvertible
-    {
-        if (!typeof(T).IsEnum)
-        {
-            throw new ArgumentException(
-                string.Format(_argumentExceptionFormat, typeof(T)));
-        }
+	public static bool HasNext<T>(this T src) where T : struct, IConvertible
+	{
+		if (!typeof(T).IsEnum)
+		{
+			throw new ArgumentException(
+				string.Format(_argumentExceptionFormat, typeof(T)));
+		}
 
-        var array = (T[]) Enum.GetValues(src.GetType());
-        int j = Array.IndexOf(array, src) + 1;
+		var array = (T[]) Enum.GetValues(src.GetType());
+		int j = Array.IndexOf(array, src) + 1;
 
-        return j < array.Length;
-    }
+		return j < array.Length;
+	}
 
-    public static T Next<T>(this T src) where T : struct, IConvertible
-    {
-        if (!typeof(T).IsEnum)
-        {
-            throw new ArgumentException(
-                string.Format(_argumentExceptionFormat, typeof(T)));
-        }
+	public static T Next<T>(this T src) where T : struct, IConvertible
+	{
+		if (!typeof(T).IsEnum)
+		{
+			throw new ArgumentException(
+				string.Format(_argumentExceptionFormat, typeof(T)));
+		}
 
-        var array = (T[]) Enum.GetValues(src.GetType());
-        int j = Array.IndexOf(array, src) + 1;
-        return (array.Length == j) ? array[0] : array[j];
-    }
+		var array = (T[]) Enum.GetValues(src.GetType());
+		int j = Array.IndexOf(array, src) + 1;
+		return (array.Length == j) ? array[0] : array[j];
+	}
 
-    public static bool HasPrev<T>(this T src) where T : struct, IConvertible
-    {
-        if (!typeof(T).IsEnum)
-        {
-            throw new ArgumentException(
-                string.Format(_argumentExceptionFormat, typeof(T)));
-        }
+	public static bool HasPrev<T>(this T src) where T : struct, IConvertible
+	{
+		if (!typeof(T).IsEnum)
+		{
+			throw new ArgumentException(
+				string.Format(_argumentExceptionFormat, typeof(T)));
+		}
 
-        var array = (T[]) Enum.GetValues(src.GetType());
-        int j = Array.IndexOf(array, src) - 1;
+		var array = (T[]) Enum.GetValues(src.GetType());
+		int j = Array.IndexOf(array, src) - 1;
 
-        return j >= 0;
-    }
+		return j >= 0;
+	}
 
-    public static T Prev<T>(this T src) where T : struct, IConvertible
-    {
-        if (!typeof(T).IsEnum)
-        {
-            throw new ArgumentException(
-                string.Format(_argumentExceptionFormat, typeof(T)));
-        }
+	public static T Prev<T>(this T src) where T : struct, IConvertible
+	{
+		if (!typeof(T).IsEnum)
+		{
+			throw new ArgumentException(
+				string.Format(_argumentExceptionFormat, typeof(T)));
+		}
 
-        var array = (T[]) Enum.GetValues(src.GetType());
-        int j = Array.IndexOf(array, src) - 1;
+		var array = (T[]) Enum.GetValues(src.GetType());
+		int j = Array.IndexOf(array, src) - 1;
 
-        return (j == -1)
-            ? array[array.Length - 1]
-            : array[j];
-    }
+		return (j == -1)
+			? array[array.Length - 1]
+			: array[j];
+	}
 }
